f7dbc67b3bb78e2432d9243794873a75f2b870ae
Lindsey Gray <lindsey.gray@gmail.com>
2013-07-02 05:37:33 -0500

    remove debug

--- python/ShapeTools.py
+++ python/ShapeTools.py
@@ -73,8 +73,6 @@ class ShapeBuilder(ModelBuilder):
                 if b in self.pdfModes: 
                     pdf_s.setAttribute('forceGen'+self.pdfModes[b].title())
                     pdf_b.setAttribute('forceGen'+self.pdfModes[b].title())
-                sumPlusNuis_s.Print("V")
-                pdf_s.Print("V")
                 self.out._import(pdf_s, ROOT.RooFit.RenameConflictNodes(b))
                 self.out._import(pdf_b, ROOT.RooFit.RecycleConflictNodes(), ROOT.RooFit.Silence())
             else:
